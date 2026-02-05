e61d274f6da4ec1700d4ec6dbfd91933c4d08268
aravinthan.n <aravinthan.n@igosolutions.eu>
2026-02-05 10:49:56 +0530

    nes

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,14 @@ import React from 'react';
 import App from './App.jsx'
 import './index.css';
 import NewTab from './pages/newtab.jsx';
-import { BrowserRouter,Routes,Route,HashRouter } from 'react-router-dom';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
 <StrictMode>
-  <HashRouter>
   <BrowserRouter basename="/newsletter-app_v3">
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/Articles" element={<NewTab />} />
     </Routes>
   </BrowserRouter>
-  </HashRouter>
-
 </StrictMode>
 );
